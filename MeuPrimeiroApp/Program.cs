3e349ee2a8e5fcb1c68c9f469d829fe72839b4b6
Lucas Machado <lucasmachadocbez@gmail.com>
2025-10-02 12:00:17 -0300

    Primeiro commit

--- MeuPrimeiroApp/Program.cs
+++ MeuPrimeiroApp/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Screen Sound
+string mensagemDeBoasVindas = "Boas Vindas ao Screen Sound";
+Console.WriteLine(mensagemDeBoasVindas);
+ 
