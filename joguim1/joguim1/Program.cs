da0a13905d0126fb53ed70aaa5b445b3699104c7
lucas machado <lucasmachadocbez@gmail.com>
2025-10-02 21:21:36 -0300

    Joguim 1

---
+++ joguim1/joguim1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+Random aleatorio = new Random();
+int secretNumber = aleatorio.Next(1, 101);
+
+do
+{
+    Console.Write("Mo fi agora tu escolhes um numero entre 1 e 100");
+    int chute = int.Parse(Console.ReadLine());
+
+    if (chute == secretNumber)
+    {
+        Console.Write("Acerto miseravi");
+        break;
+    }
+    else if (chute < secretNumber)
+    {
+        Console.Write("abaixo do esperado");
+    }
+    else if (chute > secretNumber)
+    {
+        Console.Write("Acima do esperado");
+    }
+
+
+
+} while (true);
+
+Console.Write("O jogo acabou e voce venceeeeu !!! PARABENS!!!");
+
+
