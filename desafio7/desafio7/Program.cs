87c72bf025220860d807b57472333db5264eeb9b
lucas machado <lucasmachadocbez@gmail.com>
2025-10-04 21:13:38 -0300

    Adicionando aula 7

---
+++ desafio7/desafio7/Program.cs
@@ -0,0 +1,23 @@
+﻿//Desafio 1
+
+
+void calculator (float a, float b)
+{
+
+    float sum = a + b;
+    float subtracao = a - b;
+    float division = a / b;
+    float multiplication = a * b;
+    Console.WriteLine($"Soma {sum}, subtracao {subtracao}, divisao {division}, multiplicacao {multiplication}");
+
+}
+
+
+float a = 10;
+
+float b = 3;
+
+calculator(a, b);
+
+
+//Desafio 2
